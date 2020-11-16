773eb8ad207e2c26db647fdd230cb93b1752b2a5
איתי שחר <itayshachar04@gmail.com>
2020-11-16 16:49:27 +0200

    Add project files.

---
+++ TicTacToe_ItayShachar_GuyWittlin/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace TicTacToe_ItayShachar_GuyWittlin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gameboard = new System.Windows.Forms.PictureBox();
+            this.sign1 = new System.Windows.Forms.PictureBox();
+            this.sign2 = new System.Windows.Forms.PictureBox();
+            this.sign3 = new System.Windows.Forms.PictureBox();
+            this.sign4 = new System.Windows.Forms.PictureBox();
+            this.sign5 = new System.Windows.Forms.PictureBox();
+            this.sign6 = new System.Windows.Forms.PictureBox();
+            this.sign7 = new System.Windows.Forms.PictureBox();
+            this.sign8 = new System.Windows.Forms.PictureBox();
+            this.sign9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Gameboard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Gameboard
+            // 
+            this.Gameboard.Image = global::TicTacToe_ItayShachar_GuyWittlin.Properties.Resources.לוח_משחק;
+            this.Gameboard.Location = new System.Drawing.Point(0, -2);
+            this.Gameboard.Name = "Gameboard";
+            this.Gameboard.Size = new System.Drawing.Size(953, 619);
+            this.Gameboard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Gameboard.TabIndex = 0;
+            this.Gameboard.TabStop = false;
+            // 
+            // sign1
+            // 
+            this.sign1.Location = new System.Drawing.Point(75, 48);
+            this.sign1.Name = "sign1";
+            this.sign1.Size = new System.Drawing.Size(192, 148);
+            this.sign1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign1.TabIndex = 1;
+            this.sign1.TabStop = false;
+            this.sign1.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign2
+            // 
+            this.sign2.Location = new System.Drawing.Point(395, 48);
+            this.sign2.Name = "sign2";
+            this.sign2.Size = new System.Drawing.Size(192, 148);
+            this.sign2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign2.TabIndex = 2;
+            this.sign2.TabStop = false;
+            this.sign2.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign3
+            // 
+            this.sign3.Location = new System.Drawing.Point(699, 48);
+            this.sign3.Name = "sign3";
+            this.sign3.Size = new System.Drawing.Size(192, 148);
+            this.sign3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign3.TabIndex = 3;
+            this.sign3.TabStop = false;
+            this.sign3.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign4
+            // 
+            this.sign4.Location = new System.Drawing.Point(75, 221);
+            this.sign4.Name = "sign4";
+            this.sign4.Size = new System.Drawing.Size(192, 138);
+            this.sign4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign4.TabIndex = 4;
+            this.sign4.TabStop = false;
+            this.sign4.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign5
+            // 
+            this.sign5.Location = new System.Drawing.Point(396, 221);
+            this.sign5.Name = "sign5";
+            this.sign5.Size = new System.Drawing.Size(192, 138);
+            this.sign5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign5.TabIndex = 5;
+            this.sign5.TabStop = false;
+            this.sign5.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign6
+            // 
+            this.sign6.Location = new System.Drawing.Point(699, 221);
+            this.sign6.Name = "sign6";
+            this.sign6.Size = new System.Drawing.Size(192, 138);
+            this.sign6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign6.TabIndex = 6;
+            this.sign6.TabStop = false;
+            this.sign6.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign7
+            // 
+            this.sign7.Location = new System.Drawing.Point(75, 381);
+            this.sign7.Name = "sign7";
+            this.sign7.Size = new System.Drawing.Size(192, 148);
+            this.sign7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign7.TabIndex = 7;
+            this.sign7.TabStop = false;
+            this.sign7.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign8
+            // 
+            this.sign8.Location = new System.Drawing.Point(395, 381);
+            this.sign8.Name = "sign8";
+            this.sign8.Size = new System.Drawing.Size(192, 148);
+            this.sign8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign8.TabIndex = 8;
+            this.sign8.TabStop = false;
+            this.sign8.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // sign9
+            // 
+            this.sign9.Location = new System.Drawing.Point(699, 381);
+            this.sign9.Name = "sign9";
+            this.sign9.Size = new System.Drawing.Size(192, 148);
+            this.sign9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sign9.TabIndex = 9;
+            this.sign9.TabStop = false;
+            this.sign9.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 617);
+            this.Controls.Add(this.sign9);
+            this.Controls.Add(this.sign8);
+            this.Controls.Add(this.sign7);
+            this.Controls.Add(this.sign6);
+            this.Controls.Add(this.sign5);
+            this.Controls.Add(this.sign4);
+            this.Controls.Add(this.sign3);
+            this.Controls.Add(this.sign2);
+            this.Controls.Add(this.sign1);
+            this.Controls.Add(this.Gameboard);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Gameboard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sign9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Gameboard;
+        private System.Windows.Forms.PictureBox sign1;
+        private System.Windows.Forms.PictureBox sign2;
+        private System.Windows.Forms.PictureBox sign3;
+        private System.Windows.Forms.PictureBox sign4;
+        private System.Windows.Forms.PictureBox sign5;
+        private System.Windows.Forms.PictureBox sign6;
+        private System.Windows.Forms.PictureBox sign7;
+        private System.Windows.Forms.PictureBox sign8;
+        private System.Windows.Forms.PictureBox sign9;
+    }
+}
+
---
+++ TicTacToe_ItayShachar_GuyWittlin/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToe_ItayShachar_GuyWittlin.Properties;
+
+
+namespace TicTacToe_ItayShachar_GuyWittlin
+{
+    public partial class Form1 : Form
+    {
+        private bool m_IsFirstPlayer = true;
+
+        private Image[] m_Images = new Image[2];
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetImagesArray();
+        }
+
+        public void SetImagesArray()
+        {
+            m_Images[0] = Resources.x;
+            m_Images[1] = Resources.circle;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sign_Click(object sender, EventArgs e)
+        {
+            PictureBox sign = sender as PictureBox;
+
+            if (m_IsFirstPlayer)
+            {
+                sign.Image = Resources.x;
+                m_IsFirstPlayer = false;
+                sign.Enabled = true;
+            }
+            else
+            {
+                sign.Image = Resources.circle;
+            }
+        }
+    }
+}
