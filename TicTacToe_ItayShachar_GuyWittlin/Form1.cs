13de9788d1ee742640415d23dbd06dd5001e0449
איתי שחר <itayshachar04@gmail.com>
2020-11-23 16:47:24 +0200

    HoesMad

--- TicTacToe_ItayShachar_GuyWittlin/Form1.cs
+++ TicTacToe_ItayShachar_GuyWittlin/Form1.cs
@@ -19,7 +19,69 @@ namespace TicTacToe_ItayShachar_GuyWittlin
         private Image[] m_Images = new Image[2];
 
 
+        public string PictureToString()
+        {
+            
+        }
 
+        public bool IsImagesMatch(Image image1, Image image2)
+        {
+            try
+            {
+                //create instance or System.Drawing.ImageConverter to convert
+                //each image to a byte array
+                ImageConverter converter = new ImageConverter();
+                //create 2 byte arrays, one for each image
+                byte[] imgBytes1 = new byte[1];
+                byte[] imgBytes2 = new byte[1];
+
+                //convert images to byte array
+                imgBytes1 = (byte[])converter.ConvertTo(image1, imgBytes2.GetType());
+                imgBytes2 = (byte[])converter.ConvertTo(image2, imgBytes1.GetType());
+
+                //now compute a hash for each image from the byte arrays
+                System.Security.Cryptography.SHA256Managed sha = new System.Security.Cryptography.SHA256Managed();
+                byte[] imgHash1 = sha.ComputeHash(imgBytes1);
+                byte[] imgHash2 = sha.ComputeHash(imgBytes2);
+
+                //now let's compare the hashes
+                for (int i = 0; i < imgHash1.Length && i < imgHash2.Length; i++)
+                {
+                    //whoops, found a non-match, exit the loop
+                    //with a false value
+                    if (!(imgHash1[i] == imgHash2[i]))
+                        return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            //we made it this far so the images must match
+            return true;
+        }
+
+        public string[,] FormToMatrix()
+        {
+            //המטריצה המוחזרת
+            string[,] picturesMatrix = new string[3, 3];
+            int row, col;
+            PictureBox curPicture;
+
+            //עוברים על כלל הפקדים בטופס - נעזרים בלולאת foreach
+            foreach (Control ctrl in this.Controls)
+                if (ctrl is PictureBox)
+                {
+                    curPicture = ctrl as PictureBox;
+
+                    //מציאת מספר השורה והעמודה מתוך שם הפקד
+                    row = int.Parse(curPicture.Name[curPicture.Name.Length - 2].ToString());
+                    col = int.Parse(curPicture.Name[curPicture.Name.Length - 1].ToString());
+                    picturesMatrix[row, col] = curPicture.Text;
+                }
+            return picturesMatrix;
+        }
 
         public Form1()
         {
@@ -47,11 +109,14 @@ namespace TicTacToe_ItayShachar_GuyWittlin
             {
                 sign.Image = Resources.x;
                 m_IsFirstPlayer = false;
-                sign.Enabled = true;
+                sign.Enabled = false;
             }
             else
             {
                 sign.Image = Resources.circle;
+                m_IsFirstPlayer = true;
+                sign.Enabled = false;
+
             }
         }
     }
